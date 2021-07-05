2991e66c7e354875953bd1f565d19f37268695d3
joon <kimjune2002@naevr.com>
2021-07-05 16:33:00 +0900

    07/05 Mainscreen

---
+++ BulletTeleportShooter/Assets/메인화면/StartBtn.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartBtn : MonoBehaviour
+{
+    public void OnClickBtn()
+    {
+        SceneManager.LoadScene("Character");
+        
+        Debug.Log("다음화면");
+    }
+}
