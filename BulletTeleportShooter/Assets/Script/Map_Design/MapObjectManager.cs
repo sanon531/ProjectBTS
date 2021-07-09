0e9e8afeedecec2272bf50e7363d4630294729e7
Leejeongin <jeonginlee72@gmail.com>
2021-07-09 15:26:44 +0900

    07/09 가시 함정 시간에 따라 빠르게 올라오기

--- BulletTeleportShooter/Assets/Script/Map_Design/MapObjectManager.cs
+++ BulletTeleportShooter/Assets/Script/Map_Design/MapObjectManager.cs
@@ -7,8 +7,10 @@ public class MapObjectManager : MonoBehaviour
 
     [SerializeField]
     List <BTS_Object_Data> OnOffBlockList = new List <BTS_Object_Data> ();
+    List<BTS_Spikes_Data> SpikeList = new List<BTS_Spikes_Data>();
 
     public static MapObjectManager Instance;
+    public Animator animator;
 
     // Start is called before the first frame update
     void Awake()
@@ -21,17 +23,40 @@ public class MapObjectManager : MonoBehaviour
         OnOffBlockList.Add(data);
     }
 
+    public void AddSpike(BTS_Spikes_Data data)
+    {
+        SpikeList.Add(data);
+    }
+
     public float timer = 0f;
+
+    public float spike_timer = 0f;
+    public float spike_speed = 1f;
+
     //float timer = 0f;
     // Update is called once per frame
     void Update()
     {
+        spike_timer += Time.deltaTime;
+        
+        if (spike_timer > 5f)
+        {
+
+            spike_speed++;
+            for (int i = 0; i < SpikeList.Count; i++)
+            {
+                SpikeList[i].animator.SetFloat("spikeupd", spike_speed);
+            }
+            spike_timer = 0f;
+        }
+        
 
         //Timer Timer_GameTime = GameObject.Find("GameManager").GetComponent<Timer>();
         //timer = Timer_GameTime.GameTime;
-        
-        timer += Time.deltaTime;
 
+        //timer += Time.deltaTime;
+
+        /*
         if (timer > 5f)
         {
             for (int i = 0; i<OnOffBlockList.Count; i++)
@@ -40,6 +65,6 @@ public class MapObjectManager : MonoBehaviour
             }
             timer = 0f;
         }
-        
+        */
     }
 }
