ac339bba68264f40ab14757441223216809630fe
김수환 <reiy0902@naver.com>
2021-07-19 15:38:41 +0900

    07/19 Enemy Spawn Mechanism Changed
    최대치까지 스폰 에서 현재 인원에 대한 스폰 최대치 설정으로 변경.

--- BulletTeleportShooter/Assets/Script/KSH_AI/SpawnManager.cs
+++ BulletTeleportShooter/Assets/Script/KSH_AI/SpawnManager.cs
@@ -86,55 +86,59 @@ public class SpawnManager : MonoBehaviour
             }
             yield return null;
         }
-
-        while (spawnedEnemyCount < MAX_ENEMY_COUNT)
+        while (true)
         {
-            // 위치 기반 스폰 포인트 선별
-            float min = minDistance * minDistance;
-            float max = maxDistance * maxDistance;
-            List<Vector3> spawnPos = new List<Vector3>();
-            for(int i = 0; i < spawnPoints.Length; ++i)
+            if (spawnedEnemyCount < MAX_ENEMY_COUNT)
             {
-                float sqrDistance = (playerTransform.position - spawnPoints[i].position).sqrMagnitude;
-                if (min <= sqrDistance && sqrDistance <= max)
+                // 위치 기반 스폰 포인트 선별
+                float min = minDistance * minDistance;
+                float max = maxDistance * maxDistance;
+                List<Vector3> spawnPos = new List<Vector3>();
+                for (int i = 0; i < spawnPoints.Length; ++i)
                 {
-                    spawnPos.Add(spawnPoints[i].position);
+                    float sqrDistance = (playerTransform.position - spawnPoints[i].position).sqrMagnitude;
+                    if (min <= sqrDistance && sqrDistance <= max)
+                    {
+                        spawnPos.Add(spawnPoints[i].position);
+                    }
                 }
+
+                if (spawnPos.Count > 0)
+                {
+                    // 몇 마리를 스폰할 지 랜덤으로 결정
+                    int spawnCount = Random.Range(0, spawnPos.Count) + 1;
+                    if (spawnedEnemyCount + spawnCount > MAX_ENEMY_COUNT) spawnCount = MAX_ENEMY_COUNT - spawnedEnemyCount;
+
+                    // 어느 곳에 스폰할 지 랜덤으로 결정 (중복 불가)
+                    int[] spawnPointIndex = new int[spawnPos.Count];
+                    for (int i = 0; i < spawnPointIndex.Length; ++i) spawnPointIndex[i] = i;
+                    for (int i = 0; i < spawnPointIndex.Length - 1; ++i)
+                    {
+                        int randIndex = Random.Range(0, spawnPointIndex.Length);
+                        int tempValue = spawnPointIndex[i];
+                        spawnPointIndex[i] = spawnPointIndex[randIndex];
+                        spawnPointIndex[randIndex] = tempValue;
+                    }
+
+                    // 어떤 적을 스폰할 지 랜덤으로 결정 (중복 가능)
+                    int[] spawnEnemyIndex = new int[spawnCount];
+                    for (int i = 0; i < spawnCount; ++i) spawnEnemyIndex[i] = Random.Range(0, spawnEnemies.Length);
+
+                    // 적을 스폰
+                    for (int i = 0; i < spawnCount; ++i)
+                    {
+                        Enemy newEnemy = Instantiate(spawnEnemies[spawnEnemyIndex[i]], spawnPos[spawnPointIndex[i]], Quaternion.identity, transform).Init();
+                        newEnemy.Attack += currentPowerUpATK;
+                        newEnemy.MaxHP += currentPowerUpHP;
+                        newEnemy.Speed += currentPowerUpSPD;
+                        newEnemy.onDeath += () => { spawnedEnemyCount--; Debug.Log(newEnemy.name + " DEAD"); };
+                    }
+
+                    spawnedEnemyCount += spawnCount;
+                }
+                yield return new WaitForSeconds(spawnDelay);
             }
-
-            if(spawnPos.Count > 0)
-            {
-                // 몇 마리를 스폰할 지 랜덤으로 결정
-                int spawnCount = Random.Range(0, spawnPos.Count) + 1;
-                if (spawnedEnemyCount + spawnCount > MAX_ENEMY_COUNT) spawnCount = MAX_ENEMY_COUNT - spawnedEnemyCount;
-
-                // 어느 곳에 스폰할 지 랜덤으로 결정 (중복 불가)
-                int[] spawnPointIndex = new int[spawnPos.Count];
-                for (int i = 0; i < spawnPointIndex.Length; ++i) spawnPointIndex[i] = i;
-                for (int i = 0; i < spawnPointIndex.Length - 1; ++i)
-                {
-                    int randIndex = Random.Range(0, spawnPointIndex.Length);
-                    int tempValue = spawnPointIndex[i];
-                    spawnPointIndex[i] = spawnPointIndex[randIndex];
-                    spawnPointIndex[randIndex] = tempValue;
-                }
-
-                // 어떤 적을 스폰할 지 랜덤으로 결정 (중복 가능)
-                int[] spawnEnemyIndex = new int[spawnCount];
-                for (int i = 0; i < spawnCount; ++i) spawnEnemyIndex[i] = Random.Range(0, spawnEnemies.Length);
-
-                // 적을 스폰
-                for (int i = 0; i < spawnCount; ++i)
-                {
-                    Enemy newEnemy = Instantiate(spawnEnemies[spawnEnemyIndex[i]], spawnPos[spawnPointIndex[i]], Quaternion.identity, transform).Init();
-                    newEnemy.Attack += currentPowerUpATK;
-                    newEnemy.MaxHP += currentPowerUpHP;
-                    newEnemy.Speed += currentPowerUpSPD;
-                }
-
-                spawnedEnemyCount += spawnCount;
-            }
-            yield return new WaitForSeconds(spawnDelay);
+            else yield return null;
         }
     }
 
