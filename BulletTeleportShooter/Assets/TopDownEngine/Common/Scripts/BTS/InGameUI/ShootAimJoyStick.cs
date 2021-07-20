bf78781be50647a72ede2514b18f6322ce6cf869
박혜인 <ms8370@naver.com>
2021-07-21 05:48:24 +0900

    07/21 점수 공식 일부 추가, 인게임UI 모바일화
    점수공식 : 시간 증가에 따른 상승, 골드(아이템) 추가
    (몬스터를 죽일 경우 점수 추가되는 공식은 몹 담당 쪽에 추가 요청할 예정)
    
    플레이어 이동 조이스틱 추가
    총 조준 조이스틱 추가
    조준 조이스틱에서 손 뗐을 때 총 발사 되도록 구현

---
+++ BulletTeleportShooter/Assets/TopDownEngine/Common/Scripts/BTS/InGameUI/ShootAimJoyStick.cs
@@ -0,0 +1,23 @@
+﻿using MoreMountains.TopDownEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace MoreMountains.Tools
+{
+    public class ShootAimJoyStick : MMTouchJoystick
+    {
+        public override void OnEndDrag(PointerEventData eventData)
+		{
+			base.OnEndDrag(eventData);
+			InputManager.Instance.ShootButtonDown();
+			Invoke("ShootStop", 0.001f);
+		}
+
+		private void ShootStop()
+        {
+			InputManager.Instance.ShootButtonUp();
+		}
+	}
+}
