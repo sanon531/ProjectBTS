5ddaf1236649a37218d35be39baaaec3f1a0e54d
joon <kimjune2002@naevr.com>
2021-07-23 00:24:40 +0900

    07/23 메인화면 개선 미완성

---
+++ BulletTeleportShooter/Assets/Script/UI_Development/Main.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+
+
+public class Main : MonoBehaviour
+{
+    public Image image;
+    public RectTransform rectTransform;
+    public GameObject MainMenu;
+    public Image[] node;
+    public GameObject[] index;
+    public RectTransform rects;
+    public Ease current_Ease = Ease.InQuad;
+    public Ease cur_Ease = Ease.InQuad;
+
+
+
+
+
+
+
+    public void Press()
+    {
+        image.DOFade(0f, 1f);
+        Button btn = GetComponent<Button>();
+        btn.enabled = false;
+        rectTransform.DOAnchorPosY(250f, 1f).SetEase(current_Ease);
+
+
+        MainMenuSet();   
+        
+
+    
+       
+       
+
+    }
+
+    void MainMenuSet()
+    {
+        node[0].DOFade(1f, 1f).SetDelay(1.5f);
+        node[1].DOFade(1f, 1f).SetDelay(1.5f);
+        node[2].DOFade(1f, 1f).SetDelay(1.5f);
+
+
+       
+        rects.DOAnchorPosY(-300f,1f).SetDelay(1.5f);
+
+
+
+
+
+        
+
+
+
+        Invoke("OnInvoke", 2.0f);
+
+    }
+   
+    void OnInvoke()
+    {
+        Button btn0 = index[0].GetComponent<Button>();
+        Button btn1 = index[1].GetComponent<Button>();
+        Button btn2 = index[2].GetComponent<Button>();
+
+        btn0.enabled = true;
+        btn1.enabled = true;
+        btn2.enabled = true;
+    }
+
+
+
+
+}
