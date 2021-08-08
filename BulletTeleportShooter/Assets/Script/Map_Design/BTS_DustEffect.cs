5ca091413beb2d2749e7f7cc563cf2c42bf3c308
Leejeongin <jeonginlee72@gmail.com>
2021-08-09 00:45:42 +0900

    08/09 먼지 바닥 이펙트 중간완성

---
+++ BulletTeleportShooter/Assets/Script/Map_Design/BTS_DustEffect.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BTS_DustEffect : MonoBehaviour
+{
+    public ParticleSystem DustFX;
+    public float t;
+
+    Coroutine coroutine;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(DustStart(t));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator DustStart(float time)
+    {
+        yield return new WaitForSeconds(time);
+        DustFX.Play();
+    }
+}
