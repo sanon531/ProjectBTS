5a96bdc27b93ff5f1b7f0c24be9127eb09ff8427
ohsangwon <axur123@gmail.com>
2021-08-15 09:45:47 +0900

    0815 보스 애니메이션, 트레일러용 UI작업

---
+++ BulletTeleportShooter/Assets/Script/SW_Practice/Boss_AnimationScript.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss_AnimationScript : MonoBehaviour
+{
+    [SerializeField]
+    private Animator bossAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (bossAnimator == null)
+            bossAnimator = GetComponent<Animator>();        
+    }
+
+    private void SetAllFalse()
+    {
+        bossAnimator.SetBool("IsRushWait", false);
+        bossAnimator.SetBool("IsRush", false);
+        bossAnimator.SetBool("IsLaserWait", false);
+        bossAnimator.SetBool("IsLaserShoot", false);
+        bossAnimator.SetBool("IsWalk", false);
+        bossAnimator.SetBool("IsAlive", true);
+
+    }
+
+    public void StartWalk()
+    {
+        SetAllFalse();
+        bossAnimator.SetBool("IsWalk", true);
+    }
+    public void StartRushWait()
+    {
+        SetAllFalse();
+        bossAnimator.SetBool("IsRushWait", true);
+    }
+    public void StartRush()
+    {
+        SetAllFalse();
+        bossAnimator.SetBool("IsRush", true);
+    }
+    public void StartLaserWait()
+    {
+        SetAllFalse();
+        bossAnimator.SetBool("IsLaserWait", true);
+    }
+    public void StartLaserShoot()
+    {
+        SetAllFalse();
+        bossAnimator.SetBool("IsLaserShoot", true);
+    }
+
+    public void Die()
+    {
+        SetAllFalse();
+        bossAnimator.SetBool("IsAlive", false);
+    }
+
+    //임시 코드 실험용
+
+    private void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.A))
+            StartWalk();
+
+        if (Input.GetKeyDown(KeyCode.S))
+            StartRush();
+
+        if (Input.GetKeyDown(KeyCode.Z))
+            StartLaserWait();
+
+        if (Input.GetKeyDown(KeyCode.X))
+            StartLaserShoot();
+
+        if (Input.GetKeyDown(KeyCode.C))
+            Die();
+
+
+
+    }
+
+
+
+
+
+
+}
---
+++ BulletTeleportShooter/Assets/Script/SW_Practice/CutSceneController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+
+public class CutSceneController : MonoBehaviour
+{
+
+
+
+    //rect transform Data와 연결 시켜서 리스트로 만든뒤 하나하나
+    public Transform CutSceneTransform;
+    public List<CutSceneData> cutSceneDatas = new List<CutSceneData>();
+    public static CutSceneController Instance; 
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    // 리스트 만든 다음 씬을 하나하나 연결 시키고 
+    public void CallCutScene(KeyCode keyCode)
+    {
+        switch (keyCode)
+        {
+            case KeyCode.A:
+                cutSceneDatas[0].isActive = true;
+                cutSceneDatas[0].rectTransform.DOAnchorPos(cutSceneDatas[0].ShowPos, 0.5f).
+                    OnComplete(() => cutSceneDatas[0].isActive = false).
+                    SetEase(Ease.OutBack).SetLoops(2, LoopType.Yoyo);
+                break;
+            case KeyCode.S:
+                cutSceneDatas[1].isActive = true;
+                cutSceneDatas[1].rectTransform.DOAnchorPos(cutSceneDatas[2].ShowPos, 0.5f).
+                    OnComplete(() => cutSceneDatas[0].isActive = false).
+                    SetEase(Ease.OutBack).SetLoops(2, LoopType.Yoyo);
+                break;
+            case KeyCode.D:
+                cutSceneDatas[2].isActive = true;
+                cutSceneDatas[2].rectTransform.DOAnchorPos(cutSceneDatas[2].ShowPos, 0.5f).
+                    OnComplete(() => cutSceneDatas[0].isActive = false).
+                    SetEase(Ease.OutBack).SetLoops(2, LoopType.Yoyo);
+                break;
+
+            default:
+                break;
+        }
+
+    }
+
+
+    List<T> Shuffle<T>(List<T> _list)
+    {
+        for (int i = 0; i < _list.Count; i++)
+        {
+            T temp = _list[i];
+            int randomIndex = Random.Range(i, _list.Count);
+            _list[i] = _list[randomIndex];
+            _list[randomIndex] = temp;
+        }
+        return _list;
+    }
+
+
+}
+
+[System.Serializable]
+public class CutSceneData
+{
+    public bool isActive= false;
+    public RectTransform rectTransform;
+    public Vector2 HidePos;
+    public Vector2 ShowPos;
+
+
+}
