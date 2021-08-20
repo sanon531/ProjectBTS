8b4d03bfc9df2dc8d334de04e0333cf7b39c62e7
Hyein <ms8370@naver.com>
2021-08-21 03:51:50 +0900

    08/21 텔레포트 대상 총알 표시 추가
    TeleportManager에서 설정 가능 (프리팹 적용해야함)

--- BulletTeleportShooter/Assets/TopDownEngine/Common/Scripts/BTS/Character/BulletTeleportManager.cs
+++ BulletTeleportShooter/Assets/TopDownEngine/Common/Scripts/BTS/Character/BulletTeleportManager.cs
@@ -8,6 +8,57 @@ namespace MoreMountains.TopDownEngine
     {
         public LinkedList<GameObject> BulletStack = new LinkedList<GameObject>();
 
+        [SerializeField] private bool UseTargetBulletIcon = false;
+
+        [SerializeField] private GameObject TargetIcon;
+        [Range(0, 2)]
+        [SerializeField] private float TargetIconSize = 1f;
+
+        
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            if (UseTargetBulletIcon)
+            {
+                TargetIcon.transform.localScale *= TargetIconSize;
+            }
+            
+        }
+
+        private void Update()
+        {
+            if (UseTargetBulletIcon)
+            {
+                ResetTargetIcon();
+            }
+        }
+
+        public void AddBullet(GameObject targetBullet)
+        {
+            BulletStack.AddLast(targetBullet);
+        }
+
+
+        public void DeleteBullet(GameObject targetBullet)
+        {
+            BulletStack.Remove(targetBullet);
+        }
+
+        private void ResetTargetIcon()
+        {
+            if (BulletStack.Last != null)
+            {
+                TargetIcon.SetActive(true);
+                TargetIcon.transform.position = BulletStack.Last.Value.transform.position;
+            }
+            else
+            {
+                TargetIcon.SetActive(false);
+            }
+        }
+
         public void printStack()
         {
             foreach (GameObject i in BulletStack)
